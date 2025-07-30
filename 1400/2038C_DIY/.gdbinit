ff1d4ed1978666db2b55951da8ed6cf9553a380d
Junu Park <junu@Junus-Mac-mini.local>
2025-07-30 23:02:35 +0900

    Still working on 2038C_DIY

--- 1400/2038C_DIY/.gdbinit
+++ 1400/2038C_DIY/.gdbinit
@@ -1,4 +1,6 @@
-b main.cpp:13 if t == 1
+b main.cpp:21 if t == 1
+b main.cpp:55
 r < input
 p t
 s
+n
--- 1400/2038C_DIY/main.cpp
+++ 1400/2038C_DIY/main.cpp
@@ -11,6 +11,7 @@ typedef vector<int> vec;
 typedef pair<int, int> pi;
 
 void solve();
+void result(int, int, int, int);
 
 int main() {
   ios::sync_with_stdio(0);
@@ -23,5 +24,49 @@ int main() {
 }
 
 void solve() {
+	int n, i;
+	cin >> n;
+	int a[n + 1];
+	for (i = 1; i <= n; ++i) {
+		cin >> a[i];
+	}
 
+	int min = INT_MAX, max = INT_MIN, prev_min = INT_MAX, prev_max = INT_MIN;
+	unordered_map<int, int> map;
+	for (i = 1; i <= n; ++i) {
+		++map[a[i]];
+	}
+	vector<int> v;
+	for (auto e : map) {
+		if (v.size() >= 4 && e.second >= 2) {
+			v.emplace_back(e.first);
+		} else {
+			for (i = 0; i < e.second / 2; ++i) {
+			v.emplace_back(e.first);
+			}
+		}
+	}
+	if (v.size() < 4) {
+		//Area of rectangle is 0 or it is degenerate.
+		cout << "NO\n";
+		return;
+	}
+
+	sort(v.begin(), v.end());
+	max = v[v.size() - 1];
+	min = v[0];
+	prev_min = v[1];
+	prev_max = v[v.size() - 2];
+
+	if ((max - min) * (prev_max - prev_min) > (max - prev_min) * (prev_max - min)) {
+		result(max, min, prev_max, prev_min);
+	} else {
+		result(max, prev_min, prev_max, min);
+	}
+}
+
+void result(int a, int b, int c, int d) {
+	cout << "YES\n";
+	cout << a << ' ' << c << ' ' << a << ' ' << d << ' ';
+	cout << b << ' ' << c << ' ' << b << ' ' << d << '\n';
 }
